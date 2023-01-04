7f71c6551c6f8d73eff9fbc2295a753959174bc3
Manuel Puentes <manuhank@gmail.com>
2023-01-04 19:07:39 -0300

    even more comments

--- index.mjs
+++ index.mjs
@@ -3,9 +3,14 @@ import { db } from "./db/connection.mjs";
 import InitializeModels from "./db/Models.mjs";
 import { recursivelySaveToDB } from "./core/fsTree.mjs";
 
+//initializing DB
 await db.authenticate();
 const { File, Folder } = await InitializeModels(db);
 await db.sync();
+
+//getting path from args
 const rootPath = process.argv[3] ?? "/";
+
+//creating root node and recursively saving it's content
 const rootFolder = await Folder.create({name: rootPath, fullPath: rootPath})
 await recursivelySaveToDB(File, Folder, rootPath, rootFolder.id);
