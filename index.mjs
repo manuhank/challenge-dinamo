8cdde3e99cf92702c88ad5f3dba0fdd94238ce8d
Manuel Puentes <manuhank@gmail.com>
2023-01-04 16:15:58 -0300

    read contents of folder and structure them

--- index.mjs
+++ index.mjs
@@ -1,11 +1,13 @@
 import "dotenv/config.js";
 import { db } from "./db/index.mjs";
 import Models from "./db/Models.mjs";
+import { getContent } from "./lib/fsTree.mjs";
 import { recSave, test } from "./test/test.mjs";
 
-await db.authenticate();
-const { File, Folder } = await Models(db);
-await db.sync();
-// const folder = await Folder.create({name: "test"});
-// console.log(folder.id)
-await recSave((o)=>Folder.create(o), test);
+// await db.authenticate();
+// const { File, Folder } = await Models(db);
+// await db.sync();
+
+const dir = await getContent("/Users/manuhank");
+console.log("dir",JSON.stringify(dir))
+// await recSave(Folder, File, test);
