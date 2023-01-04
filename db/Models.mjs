923fc738d5839cdc8337042afbf3d3b7570e33ae
Manuel Puentes <manuhank@gmail.com>
2023-01-04 17:04:34 -0300

    finnished implementation

--- db/Models.mjs
+++ db/Models.mjs
@@ -2,10 +2,11 @@ import { DataTypes } from "sequelize";
 export default (sequelize) => {
   const File = sequelize.define("File", {
     name: DataTypes.STRING,
-    extension: DataTypes.STRING,
+    fullPath: DataTypes.STRING,
   });
   const Folder = sequelize.define("Folder", {
     name: DataTypes.STRING,
+    fullPath: DataTypes.STRING,
   });
   Folder.hasMany(Folder);
   Folder.belongsTo(Folder);
--- index.mjs
+++ index.mjs
@@ -1,13 +1,11 @@
 import "dotenv/config.js";
 import { db } from "./db/index.mjs";
 import Models from "./db/Models.mjs";
-import { getContent } from "./lib/fsTree.mjs";
-import { recSave, test } from "./test/test.mjs";
+import { recursivelySaveToDB } from "./lib/fsTree.mjs";
 
-// await db.authenticate();
-// const { File, Folder } = await Models(db);
-// await db.sync();
-
-const dir = await getContent("/Users/manuhank");
-console.log("dir",JSON.stringify(dir))
-// await recSave(Folder, File, test);
+await db.authenticate();
+const { File, Folder } = await Models(db);
+await db.sync();
+const rootPath = ".";
+const rootFolder = await Folder.create({name: rootPath, fullPath: rootPath})
+await recursivelySaveToDB(File, Folder, rootPath, rootFolder.id);
